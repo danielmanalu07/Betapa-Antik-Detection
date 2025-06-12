2382ad661065522d73e18636b34909ce298789a9
danielmanalu07 <danielmanalu101@gmail.com>
2025-06-12 16:11:35 +0700

    Update Code

--- app.py
+++ app.py
@@ -108,4 +108,4 @@ def index():
         return jsonify({"error": "gagal memproses gambar", "detail": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
